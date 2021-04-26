de0a9b1c33835d224703ad811dfb04e6afac185a
ndoom90 <ndoom90@gmail.com>
2021-04-26 19:02:15 +0900

    Fail test code

--- test/movies.e2e-spec.ts
+++ test/movies.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { MoviesModule } from '../../src/movies/movies.module';
+import { MoviesModule } from "../../src/movies/movies.module";
 import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
